88158e0a5176f3e2d910a3fcb17ab837a4f44dd9
Michael McCulloch <michael.james.mcculloch@fastmail.com>
2021-04-25 08:10:17 -0600

    Remove websocket stuff; tidy imports

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,5 @@
 use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 use actix_cors::Cors;
 use actix_web::middleware;
--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,21 +1,13 @@
-use actix::prelude::*;
 use actix_web::{
     web::{self, Data, Path},
-    Error, HttpRequest, HttpResponse, Responder,
+    HttpResponse, Responder,
 };
-use actix_web_actors::ws;
 use std::sync::Mutex;
-use std::time::{Duration, Instant};
 
 extern crate common;
 use common::MyJsonFile;
 
 use super::broadcaster::Broadcaster;
-
-/// How often heartbeat pings are sent
-const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
-/// How long before lack of client response causes a timeout
-const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
 async fn new_client(broadcaster: Data<Mutex<Broadcaster>>) -> impl Responder {
     let rx = broadcaster.lock().unwrap().new_client();
@@ -29,82 +21,6 @@ async fn broadcast(msg: Path<String>, broadcaster: Data<Mutex<Broadcaster>>) -> 
     broadcaster.lock().unwrap().send(&msg.into_inner());
 
     HttpResponse::Ok().body("msg sent")
-}
-
-/// do websocket handshake and start `MyWebSocket` actor
-async fn ws_index(r: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    println!("{:?}", r);
-    let res = ws::start(MyWebSocket::new(), &r, stream);
-    println!("{:?}", res);
-    res
-}
-
-/// websocket connection is long running connection, it easier
-/// to handle with an actor
-struct MyWebSocket {
-    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
-    /// otherwise we drop connection.
-    hb: Instant,
-}
-
-impl Actor for MyWebSocket {
-    type Context = ws::WebsocketContext<Self>;
-
-    /// Method is called on actor start. We start the heartbeat process here.
-    fn started(&mut self, ctx: &mut Self::Context) {
-        self.hb(ctx);
-    }
-}
-
-/// Handler for `ws::Message`
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWebSocket {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        // process websocket messages
-        println!("WS: {:?}", msg);
-        match msg {
-            Ok(ws::Message::Ping(msg)) => {
-                self.hb = Instant::now();
-                ctx.pong(&msg);
-            }
-            Ok(ws::Message::Pong(_)) => {
-                self.hb = Instant::now();
-            }
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            Ok(ws::Message::Close(reason)) => {
-                ctx.close(reason);
-                ctx.stop();
-            }
-            _ => ctx.stop(),
-        }
-    }
-}
-
-impl MyWebSocket {
-    fn new() -> Self {
-        Self { hb: Instant::now() }
-    }
-
-    /// helper method that sends ping to client every second.
-    ///
-    /// also this method checks heartbeats from client
-    fn hb(&self, ctx: &mut <Self as Actor>::Context) {
-        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
-            // check client heartbeats
-            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
-                // heartbeat timed out
-                println!("Websocket Client heartbeat failed, disconnecting!");
-
-                // stop actor
-                ctx.stop();
-
-                // don't try to send a ping
-                return;
-            }
-
-            ctx.ping(b"");
-        });
-    }
 }
 
 pub fn echo_json_file(item: web::Json<MyJsonFile>) -> HttpResponse {
@@ -121,7 +37,6 @@ pub fn get_json_file() -> HttpResponse {
 }
 
 pub fn config_server(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/websocket").route(web::get().to(ws_index)));
     cfg.service(web::resource("/json_post").route(web::post().to(echo_json_file)));
     cfg.service(web::resource("/json_get").route(web::get().to(get_json_file)));
     cfg.service(web::resource("/events").route(web::get().to(new_client)));
